f57b66988b3d42940b4a568bdef25e10d4bcc1ee
Hemanth Nandakumar <hemanthnov2001@gmail.com>
2025-09-01 20:11:56 -0400

    added project files

---
+++ script.js
@@ -0,0 +1,24 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+const modal = document.getElementById("resumeModal");
+const btn = document.getElementById("resumeBtn");
+const span = document.querySelector(".close");
+
+btn.onclick = function() {
+  modal.style.display = "flex";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
